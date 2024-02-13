73f9eb0ad40e5feada17172a3b97d343960043dc
kangchan <phw2086@naver.com>
2024-02-13 23:05:08 +0900

    수정 : 탬플릿 약간 수정

--- src/main/java/co/hyunwoo/introduceprj/controller/WebController.java
+++ src/main/java/co/hyunwoo/introduceprj/controller/WebController.java
@@ -1,16 +1,14 @@
 package co.hyunwoo.introduceprj.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-	
-	@RequestMapping("home")
+public class WebController {
+
+	@RequestMapping("")
 	public String home(Model model) {	
 		return "home";
 	}
-	
 }
